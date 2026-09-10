5ab2ce2e1c209688c6f90848f152ee589b097230
mouadziyani <ziyanimouad@gmail.com>
2026-09-10 11:26:55 +0100

    feat : add logic modal like name owner and 6 boolean for rules

---
+++ src/main/java/ma/youcode/lineperm/model/FichierProtege.java
@@ -0,0 +1,63 @@
+package ma.youcode.lineperm.model;
+
+public class FichierProtege {
+
+    private String nameOfFile;
+    private String owner;
+    private boolean ownerR;
+    private boolean ownerW;
+    private boolean ownerD;
+    private boolean otherR;
+    private boolean otherW;
+    private boolean otherD;
+
+
+    public FichierProtege(String name, String owner) {
+
+        this.nameOfFile = name;
+        this.owner = owner;
+        this.ownerR = true;
+        this.ownerW = true;
+        this.ownerD = true;
+        this.otherR = false;
+        this.otherW = false;
+        this.otherD = false;
+    }
+
+
+    public String getNameOfFile() {
+        return this.nameOfFile;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public boolean getOwnerR() {
+        return this.ownerR;
+    }
+    public boolean getOwnerW() {
+        return this.ownerW;
+    }
+    public boolean getOwnerD() {
+        return this.ownerD;
+    }
+    public boolean getOtherR() {
+        return this.otherR;
+    }
+    public boolean getOtherW() {
+        return this.otherW;
+    }
+    public boolean getOtherD() {
+        return this.otherD;
+    }
+    public void setotherR(boolean otherR) {
+        this.otherR = otherR;
+    }
+    public void setotherW(boolean otherW) {
+        this.otherW = otherW;
+    }
+    public void setotherD(boolean otherD) {
+        this.otherD = otherD;
+    }
+}
