34ef63279521e111a5c787a7a8f9aed7a619c7e4
mouadziyani <ziyanimouad@gmail.com>
2026-09-11 09:26:41 +0100

    feat : create le file FileServices and logic for commande touch

---
+++ src/main/java/ma/youcode/lineperm/service/FileService.java
@@ -0,0 +1,58 @@
+package ma.youcode.lineperm.service;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import ma.youcode.lineperm.model.FichierProtege;
+
+public class FileService{
+
+    Path dossier = Path.of("createdFile");  
+    private List<FichierProtege> fichiers = new ArrayList<>();
+
+    public FileService(){
+
+    }
+
+   public boolean touch(String name, String owner) {
+
+       
+       try {
+           
+           if (name == null || name.isEmpty()) {
+               return false;
+           }
+    
+           for (FichierProtege fichier : fichiers) {
+               if (fichier.getNameOfFile().equals(name)) {
+                   return false;
+               }
+           }
+            if (!Files.exists(dossier)) {
+                Files.createDirectory(dossier);
+            }
+
+            
+                Path file = dossier.resolve(name);
+
+                Files.createFile(file);
+
+                FichierProtege newFile = new FichierProtege(name, owner);
+
+                String filePermWriter = "rwd|--- " + owner + " " + name;
+
+                System.out.println(filePermWriter);
+
+                fichiers.add(newFile);
+
+                return true ;
+
+        } catch (Exception e) {
+
+            System.out.println("error");
+            return false;
+        }
+    }
+}
