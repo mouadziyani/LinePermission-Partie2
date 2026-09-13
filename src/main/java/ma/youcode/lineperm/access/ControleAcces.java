a2b6cb5bcc3f730f08b705d70dedac7f376562dd
mouadziyani <ziyanimouad@gmail.com>
2026-09-13 19:55:57 +0100

    feat(access): add file permission access controls

--- src/main/java/ma/youcode/lineperm/access/ControleAcces.java
+++ src/main/java/ma/youcode/lineperm/access/ControleAcces.java
@@ -1,5 +1,35 @@
 package ma.youcode.lineperm.access;
 
+import ma.youcode.lineperm.model.FichierProtege;
+
 public class ControleAcces {
-    
+
+    public static boolean canRead(FichierProtege fichier, String username) {
+
+        if (fichier.getOwner().equals(username)) {
+            return fichier.getOwnerR();
+        }
+
+        return fichier.getOtherR();
+    }
+
+
+    public static boolean canWrite(FichierProtege fichier, String username) {
+
+        if (fichier.getOwner().equals(username)) {
+            return fichier.getOwnerW();
+        }
+
+        return fichier.getOtherW();
+    }
+
+
+    public static boolean canDelete(FichierProtege fichier, String username) {
+
+        if (fichier.getOwner().equals(username)) {
+            return fichier.getOwnerD();
+        }
+
+        return fichier.getOtherD();
+    }
 }
