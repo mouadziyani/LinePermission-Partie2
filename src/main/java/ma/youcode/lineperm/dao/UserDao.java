58bea9c85c71659481d8ba3391a8dd2dc28de3cf
mouadziyani <ziyanimouad@gmail.com>
2026-09-25 09:29:39 +0000

    feat : Overridde save methode and add logic for insert data

--- src/main/java/ma/youcode/lineperm/dao/UserDao.java
+++ src/main/java/ma/youcode/lineperm/dao/UserDao.java
@@ -6,8 +6,19 @@ import ma.youcode.lineperm.model.Users;
 
 public class UserDao extends AbstractDao<Users> {
 
+    private static final String insertQuery =
+            "INSERT INTO users(username, password) VALUES (?, ?)";
+
     @Override
-    public void save(Users t) {
+    public void save(Users user) {
+        try (PreparedStatement preparedStatement = connect.prepareStatement(insertQuery)){
+            preparedStatement.setString(1,user.getUsername());
+            preparedStatement.setString(2,user.getPasswordHash());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     @Override
