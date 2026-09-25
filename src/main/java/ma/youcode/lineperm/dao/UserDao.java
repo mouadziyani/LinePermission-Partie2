9dda0943e1d63dfbdd7d26ceb10ecdf7fbc04543
mouadziyani <ziyanimouad@gmail.com>
2026-09-25 09:28:05 +0000

    feat : create UserDao

---
+++ src/main/java/ma/youcode/lineperm/dao/UserDao.java
@@ -0,0 +1,23 @@
+package ma.youcode.lineperm.dao;
+
+import java.sql.*;
+
+import ma.youcode.lineperm.model.Users;
+
+public class UserDao extends AbstractDao<Users> {
+
+    @Override
+    public void save(Users t) {
+    }
+
+    @Override
+    public Users findById(int id) {
+        return null ;
+    }
+
+    @Override
+    public void delete(int id) {
+    }
+
+    
+}
