fa35552f5ab10e9bdb4b4724d35d6d62363d2d38
mouadziyani <ziyanimouad@gmail.com>
2026-09-25 09:30:46 +0000

    feat : Overridde findById methode and add logic for isearch user with her Id

--- src/main/java/ma/youcode/lineperm/dao/UserDao.java
+++ src/main/java/ma/youcode/lineperm/dao/UserDao.java
@@ -8,6 +8,9 @@ public class UserDao extends AbstractDao<Users> {
 
     private static final String insertQuery =
             "INSERT INTO users(username, password) VALUES (?, ?)";
+    
+    private static final String deleteQuery =
+            "DELETE FROM users WHERE id = ?";
 
     @Override
     public void save(Users user) {
@@ -23,6 +26,22 @@ public class UserDao extends AbstractDao<Users> {
 
     @Override
     public Users findById(int id) {
+        try(PreparedStatement preparedStatement = connect.prepareStatement(findByIdQuery)){
+            preparedStatement.setInt(1, id);
+
+            try(ResultSet result = preparedStatement.executeQuery()){
+                if (result.next()) {
+                    String username = result.getString("username");
+                    String passwordHash = result.getString("password");
+
+                    return new Users(username, passwordHash);
+                }
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
         return null ;
     }
 
